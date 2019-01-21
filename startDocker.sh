f8334dde0a916f82a231cc632a9fe9a3780692de
Sandro Eitzinger <sandro.eitzinger@student.tugraz.at>
2019-01-21 18:53:40 +0100

    Fix KVM Permissions

--- startDocker.sh
+++ startDocker.sh
@@ -2,6 +2,6 @@
 VERSION="$(md5sum docker/Dockerfile.jenkins | awk '{print $1}')"
 WORKINGDIR="$(pwd)"
 
-docker build . -t paintroid:${VERSION} -f docker/Dockerfile.jenkins --build-arg USER_ID=1000 --build-arg GROUP_ID=1000 --build-arg KVM_GROUP_ID=112
+docker build . -t paintroid:${VERSION} -f docker/Dockerfile.jenkins --build-arg USER_ID=1000 --build-arg GROUP_ID=1000 --build-arg KVM_GROUP_ID=$(getent group kvm | cut -d: -f3)
 
-docker run -it --rm -u 1000:1000 --device=/dev/kvm:/dev/kvm:rw -e JENKINS_URL="somecrap" -v $HOME/.gradle:/home/user/.gradle -w ${WORKINGDIR} -v ${WORKINGDIR}:${WORKINGDIR}:rw,z paintroid:${VERSION}
+docker run -it --rm -u 1000:1000 -v /dev/kvm:/dev/kvm:Z -e JENKINS_URL="notEmpty" -v $HOME/.gradle:/home/user/.gradle -w ${WORKINGDIR} -v ${WORKINGDIR}:${WORKINGDIR}:rw,z paintroid:${VERSION}
